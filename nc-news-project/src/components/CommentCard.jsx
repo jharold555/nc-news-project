697b2656f70663aea8ecb784336ff9af8ec3279b
Jasmine <jasmine.mjharold@gmail.com>
2024-05-10 12:58:17 +0100

    add error handling

--- nc-news-project/src/components/CommentCard.jsx
+++ nc-news-project/src/components/CommentCard.jsx
@@ -8,15 +8,22 @@ const CommentCard = ({ commentData }) => {
   const { username } = useContext(UserContext);
   const comment_id = commentData.comment_id;
   const [deleted, setDeleted] = useState(false);
-  const [hidden, setHidden] = useState(false);
+  const [succsessfulDelete, setSuccessfulDelete] = useState(false);
   const [warning, setWarning] = useState(false);
+  const [isError, setIsError] = useState(false)
+  const [errorMsg, setErrorMsg] = useState('')
 
   useEffect(() => {
     if (deleted) {
       deleteComment(comment_id).then((response) => {
-        setHidden(true);
+        setSuccessfulDelete(true);
         setDeleted(false);
-      });
+      }).catch((error) => {
+      setIsError(true)
+      setSuccessfulDelete(false);
+      setErrorMsg('Request failed with code ' + error.response.data.msg)
+      })
+     
     }
   }, [deleted]);
 
@@ -24,14 +31,15 @@ const CommentCard = ({ commentData }) => {
     <>
       <span
         style={{
-          display: hidden ? "inline-block" : "none",
+          display: succsessfulDelete ? "inline-block" : "none",
           width: "300px",
           border: "solid",
         }}
       >
         <p style={{ textAlign: "center" }}>
           Comment deleted successfully. Reload by pressing the refresh button
-          below
+          below. 
+         
         </p>
         <button
           style={{ margin: "20px" }}
@@ -42,11 +50,11 @@ const CommentCard = ({ commentData }) => {
           Refresh
         </button>
       </span>
-
       <section
         className="comment-card"
-        style={{ display: hidden ? "none" : "inline-flex" }}
+        style={{ display: succsessfulDelete ? "none" : "inline-flex" }}
       >
+        <p style={{display: isError ? 'inline' : 'none', backgroundColor: 'lightgrey'}}>Comment could not be deleted, please try again! Error: {errorMsg}</p>
         <p>{commentData.body}</p>
         <h5>Votes: {commentData.votes}</h5>
         <h4>
