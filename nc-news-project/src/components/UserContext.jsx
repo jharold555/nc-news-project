02a58e27575902bedfa7379d62b1e7809aef6822
Jasmine <jasmine.mjharold@gmail.com>
2024-05-10 10:43:19 +0100

    add user context

---
+++ nc-news-project/src/components/UserContext.jsx
@@ -0,0 +1,11 @@
+import { createContext, useState } from "react";
+export const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [username, setUsername] = useState("tickle122");
+  return (
+    <UserContext.Provider value={{ username, setUsername }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
